284d7155cceade6e03507198ba4ad6e2230fa9b1
givemin5 <givemin5@gmail.com>
2019-01-23 16:19:12 +0800

    Add SpecFlow

---
+++ LeetCode771JewelsAndStones/JewelsAndStonesSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace LeetCode771JewelsAndStones
+{
+    [Binding]
+    public class JewelsAndStonesSteps
+    {
+        [Given(@"I have entered J (.*) into the solution")]
+        public void GivenIHaveEnteredJIntoTheSolution()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered S (.*) into the solution")]
+        public void GivenIHaveEnteredSIntoTheSolution()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press NumJewelsInStones")]
+        public void WhenIPressNumJewelsInStones()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ LeetCode771JewelsAndStones/SpecFlowFeature1.feature
@@ -0,0 +1,9 @@
+﻿Feature: Jewels and Stones
+
+
+@mytag
+Scenario: NumJewelsInStones
+	Given I have entered J aA into the solution
+	And I have entered S aAAbbbb into the solution
+	When I press NumJewelsInStones
+	Then the result should be 3 on the screen
