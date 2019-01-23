a1a2df163523b5d7aa888c3cf22856d192228b5d
givemin5 <givemin5@gmail.com>
2019-01-23 11:39:53 +0800

    Add project files.

---
+++ LeetCode771JewelsAndStones/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetCode771JewelsAndStones")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode771JewelsAndStones")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a8d5578-6fdf-4247-93d6-48e3a9a139e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeetCode771JewelsAndStones/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode771JewelsAndStones
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
